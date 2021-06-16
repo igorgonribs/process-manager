ba6e683875aa39c75fa2552b6211804f295aaa8a
igor <igorg.silva@bradesco.com.br>
2021-06-16 10:54:42 -0300

    Repositório criado, projeto backend adicionado

---
+++ process-manager-backend/src/main/java/com/igor/challenge/processmanagerbackend/ProcessManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.igor.challenge.processmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessManagerBackendApplication.class, args);
+	}
+
+}
---
+++ process-manager-backend/src/test/java/com/igor/challenge/processmanagerbackend/ProcessManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.challenge.processmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
